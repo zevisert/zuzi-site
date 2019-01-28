289b0790e6538613aff73f3bd68fa2608dc8f547
Zev Isert <dev@zevisert.ca>
2019-01-28 12:01:32 -0800

    Order item schema requires and min gt 0

--- server/src/models/order-item.model.mjs
+++ server/src/models/order-item.model.mjs
@@ -3,11 +3,19 @@ import mongoose from 'mongoose';
 export const orderItemSchema = new mongoose.Schema({
     quantity: {
         type: Number,
-        min: 0,
+        min: 1,
     },
     
-    item: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-    pricing: { type: mongoose.Schema.Types.ObjectId, ref: 'Pricing' }
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    pricing: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pricing',
+        required: true
+     }
 });
 
 export const OrderItem = mongoose.model('OrderItem', orderItemSchema);
